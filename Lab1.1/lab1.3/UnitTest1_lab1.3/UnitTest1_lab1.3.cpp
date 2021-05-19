da0bbc9350c8b341c677843cd43527e3d73ec7b1
hiiike <58051188+hiiike@users.noreply.github.com>
2021-05-19 23:59:48 +0300

    Add files via upload

---
+++ Lab1.1/lab1.3/UnitTest1_lab1.3/UnitTest1_lab1.3.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab1.3/FuzzyNumber.cpp"
+#include "../lab1.3/FuzzyNumber.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest13
+{
+	TEST_CLASS(UnitTest13)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			FuzzyNumber e;
+			bool h = e.Init(2, 3, 4);
+			Assert::AreEqual(h, true);
+		}
+	};
+}
